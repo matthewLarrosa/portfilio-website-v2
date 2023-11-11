d645ec68b6c8d54e8c8bd1783201f41b677b0e73
matthewLarrosa <matthewterra16@gmail.com>
2023-11-11 01:00:30 -0500

    implemented experience assests

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import "./Experience.css";
 import AMEX from "/src/assets/CompanyHeaders/AMEX.svg"
+import Group from "/src/assets/group.png"
 import ExperienceTitle from "/src/assets/TitleHeaders/ExperienceTitle.svg"
 
 function Experience() {
   return (
     <div className='expContainer'>
-        <div className='expItemsContainer'>
-            <div>
-                <img src={ExperienceTitle} alt="" />  
-            </div>
-            <div>
-                <img src={AMEX} alt="" />
-            </div>
+      <div className='experiences'>
+        <img className='experienceTitle' src={ExperienceTitle} alt="experienceTitle" />  
+        <img className='amexHeader' src={AMEX} alt="american express logo"/>
+        <img className='amexGroup' src={Group} alt="american express group picture"/>
+      </div>
+      <div className='upcomingExperiences'>
 
-        </div>
+      </div>
     </div>
   )
 }
