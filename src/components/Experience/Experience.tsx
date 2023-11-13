a7b99a23e6d99e5fe3932cb72bd4b401cb267dfa
matthewLarrosa <matthewterra16@gmail.com>
2023-11-13 12:12:59 -0500

    implemented upcoming experience section

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -3,6 +3,7 @@ import "./Experience.css";
 import AMEX from "/src/assets/CompanyHeaders/AMEX.svg"
 import Group from "/src/assets/group.png"
 import ExperienceTitle from "/src/assets/TitleHeaders/ExperienceTitle.svg"
+import UpcomingTitle from "/src/assets/TitleHeaders/UpcomingTitle.svg"
 
 function Experience() {
   return (
@@ -11,9 +12,9 @@ function Experience() {
         <img className='experienceTitle' src={ExperienceTitle} alt="experienceTitle" />  
         <img className='amexHeader' src={AMEX} alt="american express logo"/>
         <img className='amexGroup' src={Group} alt="american express group picture"/>
-      </div>
-      <div className='upcomingExperiences'>
+        <p className='amexDescription'>Month long internship at American Express, tasked with developing a React Module project tracker for Amex scrum teams with convenient features like adding and editing project details, as well as time stamps to track changes. Enhanced the user experience with seamless access to update and monitor project information, including real-time project status and updates.</p>
 
+        <img className='upcomingTitle' src={UpcomingTitle} alt="UpcomingTitle" />
       </div>
     </div>
   )
