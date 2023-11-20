436b2fb59032c9689c20848a2a2e45608bbf8b66
matthewLarrosa <matthewterra16@gmail.com>
2023-11-20 01:21:19 -0500

    education section mobile

--- src/components/Education/Fiu.tsx
+++ src/components/Education/Fiu.tsx
@@ -1,22 +1,52 @@
+import React, { useState, useEffect } from 'react';
 import "./Fiu.css";
 import Education from "/src/assets/TitleHeaders/EducationTitle.svg";
-import CrestMobile from "/src/assets/fiuCrestMobile.svg";
+import CrestMobile from "/src/assets/fiuCrestMobile.png";
 import FiuTitleMobile from "/src/assets/fiuMobile.svg";
-import init from "/src/assets/init.svg";
+import FiuTitleDesktop from "/src/assets/fiuDesktop.svg";
+import init from "/src/assets/init.png";
 import btt from "/src/assets/btt.svg";
-import hackathon from "/src/assets/hackathon.jpg"
+import hackathon from "/src/assets/shgroup.jpg"
 
 function Fiu() {
+  
+  const [imgSrc, setImgSrc] = useState(FiuTitleMobile); // Set initial source for mobile
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 1000) {
+        setImgSrc(FiuTitleMobile);
+      } else {
+        setImgSrc(FiuTitleDesktop);
+      }
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  
   return (
     <div className="sectionContainer">
       <div className="educationContainer">
         <img className="educationTitle" src={Education} alt="educationTitle" />
         <div className="fiuContainer">
           <img className="fiuCrest" src={CrestMobile} alt="FIU crest" />
-          <img className="fiuTitle" src={FiuTitleMobile} alt="FIU Title" />
+          <div className='fiuTitle'>
+            <p className='FIU'>Florida International University</p>
+            <p className='GRAD'>Graduating December 2024</p>
+          </div>
+          {/* <img className="fiuTitle" src={imgSrc} alt="FIU Title" /> */}
         </div>
-        <p>
-          I am presently a senior at Florida International University, pursuing
+        <p className="educationDescription">
+          Currently a senior at FIU, pursuing
           a Bachelor of Science in Computer Science. Throughout my college
           journey, I have actively participated in several organizations, with a
           primary focus on two key ones: BreakThroughTech MIA and INIT. In my
@@ -31,7 +61,7 @@ function Fiu() {
         </div>
         <div className="shellhacksContainer">
           <img className="shellhacksPicture" src={hackathon} alt="hackathon picture" />
-          <p>- ShellHacks 2023</p>
+          <p className="shellhacks2023">- ShellHacks 2023</p>
         </div>
       </div>
     </div>
